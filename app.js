4c6a193d5a3359b0bed484d81dcb741295d31362
Son Dao <udemydaohsn@gmail.com>
2025-02-15 13:11:22 -0700

    create api routes

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require("express");
+import express from "express";
+import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+
 const app = express();
-const port = 3000;
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.get("/", (req, res) => {
   res.send({ greeting: "hello" });
 });
 
-app.listen(port, () => {
-  console.log(`server is running on ${port}`);
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
 });
