91242531ef9ca33a6b39fe2a056cbbe8a906c06f
Son Dao <udemydaohsn@gmail.com>
2025-02-16 20:13:09 -0700

    implement authorization

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,11 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => {
-  res.send({ title: "GET all users" });
-});
-userRouter.get("/:id", (req, res) => {
-  res.send({ title: "GET user details" });
-});
+userRouter.get("/", authorize, getUsers);
+userRouter.get("/:id", authorize, getUser);
 userRouter.post("/", (req, res) => {
   res.send({ title: "CREATE new user" });
 });
